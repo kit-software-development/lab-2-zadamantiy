707010e1951e88c6604e6e89f7248113190940d3
zadamantiy <33583385+zadamantiy@users.noreply.github.com>
2019-04-02 20:11:45 +0300

    Add files via upload

--- Implementation/Organization/Department.cs
+++ Implementation/Organization/Department.cs
@@ -3,10 +3,17 @@
     /// <summary>
     ///     Скрытая реализация представления об отделе предприятия.
     /// </summary>
-    internal struct Department
+    internal struct Department : IDepartment
     {
         /*
          * TODO #2: Реализуйте интерфейс IDepartment для структуры Department
          */
+
+        public Department(string department) : this()
+        {
+            Name = department;
+        }
+
+        public string Name { get; private set; }
     }
 }
